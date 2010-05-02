3d8d713bfc89f58edc44674b6780568ec65da7e3
Sascha Depold <sascha@depold.com>
2010-05-02 15:48:11 +0200

    using kiwi for NoSpec

--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,10 @@
+var kiwi = require("kiwi")
+
 sys         = require("sys")
 aws         = require(__dirname + "/../lib/aws")
-credentials = require(__dirname + "/credentials"),
+credentials = require(__dirname + "/credentials")
 
-require("./nospec/nospec")
+kiwi.require("NoSpec")
 
 new NoSpec()
   .load(__dirname + "/s3.js")
